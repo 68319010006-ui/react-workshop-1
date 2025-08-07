d48f22dca36f51f8a9612724a7662201586dc0c4
68319010006-ui <68319010006@skntc.ac.th>
2025-08-07 17:54:02 +0700

    first commit

---
+++ src/post.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Post(props) {
+    console.log(props);
+    console.log(props.student);
+    console.log(props.score);
+  return (
+    <>
+    {/* <h2>Profile : {props.myId}</h2>
+    <p>{props.children}</p> */}
+    
+    <p>Content from Post</p>
+    <h2>Student Name : {props.student}</h2>
+    <p>Score : {props.score}</p>
+    
+    </>
+  );
+}
+
+export default Post
